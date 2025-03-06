1964ad181541113e16f7acb835dcfc05dfb87809
colenelson0 <coolboymusic64@gmail.com>
2025-03-05 23:44:00 -0800

    project creation

---
+++ PaintApp/PaintApp/PaintAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintAppApp.swift
+//  PaintApp
+//
+//  Created by Cole Nelson on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PaintAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PaintApp/PaintAppTests/PaintAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PaintAppTests.swift
+//  PaintAppTests
+//
+//  Created by Cole Nelson on 3/5/25.
+//
+
+import Testing
+@testable import PaintApp
+
+struct PaintAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
